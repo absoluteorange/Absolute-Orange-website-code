0fc011c3b8fb178015a445b3f35ca45da61c5c8d
amymvarga <amymvarga@gmail.com>
2013-04-28 16:24:13 +0100

    more tweaking

--- codeigniter/application/config/autoload.php
+++ codeigniter/application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'session', 'oauth/oauth');
+$autoload['libraries'] = array('database', 'session', 'oauth/OAuth');
 
 
 /*
