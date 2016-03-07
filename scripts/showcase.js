f125a0970e257435548130ec10e41da864579bdb
Amy Varga <info@absoluteorange.com>
2016-03-07 15:33:59 +0000

    adds showcase.js to correct folder

--- scripts/showcase.js
+++ scripts/showcase.js
@@ -1,35 +1,98 @@
 define(['jquery','lib/dom-ready', 'lib/signals'], function ($, domReady, Signals) {
     var showcase = function() {
-        var _showcaseClickedSignal = new Signals.Signal()
+        var showcaseClosedIcon = 'icon-arrow-a';
+        var showcaseOpenIcon = 'icon-arrow-a-selected';
+        var showcaseClickedSignal = new Signals.Signal()
         this.init=function(){
             $('.showcase , .showcase-image ').each(function(i) {
                 $(this).on('click', function(e) {
                     e.preventDefault();
-                    _showcaseClickedSignal.dispatch($(this));
+                    showcaseClickedSignal.dispatch($(this));
                 });
             });
         };
-        _showcaseClickedSignal.add(onClicked);
+        showcaseClickedSignal.add(onClicked);
         function onClicked(element) {
             var eleListItem = element.parents('.detail-list');
+            var showcase = eleListItem.find('h3').html();
             if (eleListItem.hasClass('selected') === false) {
-                var _showcase = eleListItem.find('h3').html();
-                $.ajax({
-                    dataType: 'html',
-                    url: '/Showcase/getShowcase', 
-                    data: {
-                        name: _showcase
-                    }
-                }).done(function (html) {
-                    eleListItem.append(html).addClass('selected');
-                });
+                closeShowcase();
+                getShowcase(showcase, eleListItem);
             } else {
-                eleListItem.find('.widget-detail').empty();
-                eleListItem.removeClass('selected');
+                closeShowcase(eleListItem, showcase);
+            }
+        };
+        function getShowcase(showcase, eleListItem) {
+            $.ajax({
+                dataType: 'html',
+                url: '/Showcase/getShowcase',
+                data: {
+                    name: showcase
+                }
+            }).done(function(html) {
+                openShowcase(html, eleListItem, showcase);
+            });
+        };
+        function openShowcase(html, eleListItem, showcase) {
+            eleListItem.append(html).addClass('selected');
+            eleListItem.find('.widget-summary').removeClass(showcaseClosedIcon).addClass(showcaseOpenIcon);
+            updateURL('add', showcase);
+            updateAnchor('add', eleListItem);
+
+        }
+        function updateURL(state, showcase) {
+            var pageTitle = document.title;
+            var currentURL = window.location.href;
+            var showcaseURL = encodeURI(showcase);
+            switch (state) {
+                case 'add':
+                    window.history.pushState({"pageTitle": pageTitle},
+                    pageTitle,
+                    currentURL+"/"+showcaseURL+"#selected-content");
+                 break;
+                 case 'remove':
+                    var newURL = currentURL.substring(0, currentURL.indexOf("/"+showcaseURL));
+                    window.history.pushState({"pageTitle": pageTitle},
+                    pageTitle,
+                    newURL);
+                 break;
+            }
+        };
+        function closeShowcase(eleListItem, showcase) {
+            if (eleListItem === undefined) {
+                getOpenShowcases();
+                return;
+            }
+            eleListItem.find('.widget-detail').empty();
+            eleListItem.removeClass('selected');
+            eleListItem.find('.widget-summary').removeClass(showcaseOpenIcon).addClass(showcaseClosedIcon);
+            updateURL('remove', showcase);
+            updateAnchor('remove', eleListItem);
+        };
+        function getOpenShowcases() {
+            $('.showcase').each(function(i) {
+                var parentListItem = $(this).parent('li');
+                if (parentListItem.hasClass('selected')) {
+                    var showcase = parentListItem.find('.widget-body').attr('title');
+                    closeShowcase(parentListItem, showcase);
+                }
+            });
+        };
+        function updateAnchor(state, eleListItem) {
+            switch (state) {
+                case 'add':
+                    var anchor = $(eleListItem).prev('a');
+                    $(anchor).attr('name', 'selected-content');
+                    $(document).scrollTop( $(anchor).offset().top );
+                break;
+                case 'remove':
+                    $(eleListItem).prev('a').attr('name', '');
+                break;
             }
         };
         domReady(this.init.bind(this));
     };
     return new showcase();
 });
+
 
